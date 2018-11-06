e7abb9d548c184418a0bbc1aab776b85facf7554
Kenwyne <knurzhan91@gmail.com>
2018-11-06 10:55:33 +0600

    First commit

---
+++ keepaliive.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests, telegram_send, datetime, time, schedule
+
+def report():
+    print('Report: Successful connection!')
+    telegram_send.send(messages=["Daily Report: Connection successful!"])
+
+
+while True:
+    r = requests.get('https://www.kcell.kz/', verify='/home/knurzhanov/Documents/kcellkz.pem')
+    print(r.status_code)
+    if r.status_code == 200:
+        # now = datetime.datetime.now()
+        schedule.every(1).hours.do(report)
+    else:
+        print('Connection lost')
+        telegram_send.send(messages=["Something went wrong!"])
+
+    schedule.run_pending()
+    time.sleep(300)
